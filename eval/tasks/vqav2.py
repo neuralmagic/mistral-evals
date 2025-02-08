94a48f8e837e1ff0f683751d127f8f8827597376
Shubhra Pandit <shubhra.pandit@gmail.com>
2025-02-08 03:53:06 +0000

    Load vqav2 directly from path

--- eval/tasks/vqav2.py
+++ eval/tasks/vqav2.py
@@ -42,8 +42,8 @@ class VQAv2(HuggingFaceEval):
         logging.basicConfig(level=logging.DEBUG)
 
         dataset = load_dataset(
-            self.dataset_name, split=self.dataset_split, trust_remote_code=True, 
-            data_dir="/network/shubhra/datasets/HuggingFaceM4/VQAv2/",
+            "/network/shubhra/datasets/HuggingFaceM4/VQAv2/"
+            split=self.dataset_split, trust_remote_code=True, 
         )
         for row in dataset:
             self.interactions.append(self._to_interaction(row))
